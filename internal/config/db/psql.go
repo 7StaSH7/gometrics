9a40f555af0ff1cc2493765239af1e7ccae8c8eb
Aleksandr Stadnik <sanchasyo@mail.ru>
2025-09-25 21:57:22 +0300

    fix

--- internal/config/db/psql.go
+++ internal/config/db/psql.go
@@ -3,6 +3,9 @@ package db
 import (
 	"context"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -16,5 +19,25 @@ func NewPostgresDriver(ctx context.Context, cfg *PostgresConfig) (*pgxpool.Pool,
 		return nil, err
 	}
 
+	if err := autoMigrate(cfg.URL); err != nil {
+		return nil, err
+	}
+
 	return pool, nil
+}
+
+func autoMigrate(dsn string) error {
+	m, err := migrate.New("file://migrations", dsn)
+	if err != nil {
+		return err
+	}
+	if err := m.Up(); err != nil {
+		if err == migrate.ErrNoChange {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
 }
